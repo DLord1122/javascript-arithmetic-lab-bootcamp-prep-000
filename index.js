ba0f4f09cab106509d36a969e04ee026d49edfa0
Dennis Lord <lorddennis1006@gmail.com>
2018-03-26 15:52:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function add(a,b){return (a+b)}
+<<<<<<< HEAD
 function subtract(a,b){return (a-b)}
 function multiply(a,b){return (a*b)}
 function divide(a,b){return (a/b)}
@@ -6,3 +7,6 @@ function inc(n){return (n+1)}
 function dec(n){return (n-1)}
 function makeInt(n){return parseInt(n,10)}
 function preserveDecimal(n) {return parseFloat(n,10)}
+=======
+function subtract(a,b){return (a-b)}
+>>>>>>> 67f25435aa3975cdd55ab7c2d77b8e8f608dc761
